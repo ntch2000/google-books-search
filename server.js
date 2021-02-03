3b83ef77ccf221721b43486f85820167a5b84498
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2021-02-03 14:51:55 -0500

    Created basic express server with mongoose connection

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+// create port
+const PORT = process.env.PORT || 3001;
+
+// Middleware code
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//mongoose connection
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/google-books-search",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+);
+
+const connection = mongoose.connect;
+
+// successful connection event listener
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected!");
+});
+
+// error connection event listener
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
+
+//create config route
+app.get("./api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+// listen on port
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost${PORT}`);
+});
